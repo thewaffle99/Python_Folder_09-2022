d0edb6c8ae11c49f9a000dd2afdd5cc4faea8149
Rene Marino <renemarino93@gmail.com>
2022-10-02 16:18:12 -0700

    For_Loop_Basics

---
+++ For_Loop_Basics_I/For_Loop_Basics_I.py
@@ -0,0 +1,41 @@
+# # Basics
+# for i in range(0, 151):
+#     print(i)
+
+# # Multiples of 5
+# for i in range(5, 5001, 5):
+
+#     print(i)
+
+# # Beep Boop if count up if /5 Beep if /10 Boop
+# for i in range(0, 101):
+#     if i % 10 == 0:
+#         print("Boop")
+#     elif i % 5 == 0:
+#         print("Beep")
+#     else:
+#         print(i)
+
+
+# # Add odd intigers from 0 -> 500,000, print the final sum
+
+# sum = 0
+# for i in range(1, 500001, 1):
+#     if i % 2 != 0:
+#         sum += i
+# print(sum)
+
+# # Count down by 4
+
+# for i in range(2020, 0, -4):
+#     print(i)
+
+# # Flexible Counter with inputs
+
+# low = 10
+# high = 80
+# mult = 2
+
+# for i in range(low, high + 1):
+#     if i % mult == 0:
+#         print(i)
